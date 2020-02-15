9f9457d4295b6d939476fe25f47e6455fd771385
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-02-15 17:52:24 -0500

    Add project files.

---
+++ Socket_Client/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Socket_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messege = new System.Windows.Forms.TextBox();
+            this.submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messege
+            // 
+            this.messege.Location = new System.Drawing.Point(45, 217);
+            this.messege.Name = "messege";
+            this.messege.Size = new System.Drawing.Size(688, 20);
+            this.messege.TabIndex = 0;
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(346, 299);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 1;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.messege);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messege;
+        private System.Windows.Forms.Button submit;
+    }
+}
+
---
+++ Socket_Client/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Socket_Client
+{
+    public partial class Form1 : Form
+    {
+        string _serverIp = "localhost";
+        int port = 8080;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            TcpClient client = new TcpClient(_serverIp, port);
+
+            int byteCount = Encoding.ASCII.GetByteCount(messege.Text); ;
+
+            byte[] sendData = new byte[byteCount];
+
+            sendData = Encoding.ASCII.GetBytes(messege.Text);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(sendData, 0, sendData.Length);
+
+            stream.Close();
+
+        }
+    }
+}
